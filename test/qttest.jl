2f5a5afbddaa520def7cf9bc7e75d2bd8e15b9b9
Isaiah Norton <isaiah.norton@gmail.com>
2014-08-17 13:43:54 -0400

    current
    Conflicts:
    	deps/BuildBootstrap.Makefile

--- test/qttest.jl
+++ test/qttest.jl
@@ -1,24 +1,25 @@
-using CXX
+require("../src/CXX.jl")
 
-const qtlibdir = "/Users/kfischer/Projects/qt-everywhere-opensource-src-5.3.1/qtbase/~/usr/lib/"
-const QtCore = joinpath(qtlibdir,"QtCore.framework/")
-const QtWidgets = joinpath(qtlibdir,"QtWidgets.framework/")
+const qtincdir = "/usr/include/qt5"
+const qtlibdir = "/usr/lib/x86_64-linux-gnu/"
 
-addHeaderDir(qtlibdir; isFramework = true, kind = C_System)
+const QtWidgets = joinpath(qtincdir,"QtWidgets/")
 
-dlopen(joinpath(QtCore,"QtCore_debug"))
-addHeaderDir(joinpath(QtCore,"Headers"))
+defineMacro("__linux")
 
-dlopen(joinpath(QtWidgets,"QtWidgets_debug"))
-addHeaderDir(joinpath(QtWidgets,"Headers"))
+addHeaderDir(qtincdir, kind = C_System)
+addHeaderDir(QtWidgets, kind = C_System)
 
-cxxinclude("QApplication")
-cxxinclude("QMessageBox")
+dlopen(joinpath(qtlibdir,"libQt5Core.so"))
+dlopen(joinpath(qtlibdir,"libQt5Gui.so"))
+
+cxxinclude("QApplication", isAngled=true)
+#cxxinclude("QtGui/qmessagebox.h")
 
 
 x = Ptr{Uint8}[pointer("julia"),C_NULL]
 app = @cxx QApplication(int32(0),pointer(x))
 
-@cxx QMessageBox::about(cast(C_NULL,pcpp"QWidget"), pointer("Hello World"), pointer("This is a QMessageBox!"))
+#@cxx QMessageBox::about(cast(C_NULL,pcpp"QWidget"), pointer("Hello World"), pointer("This is a QMessageBox!"))
 
 @cxx app->exec()
