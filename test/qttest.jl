63544e6009bbb633a4603532b08ac3972930632c
Isaiah Norton <isaiah.norton@gmail.com>
2014-08-17 13:44:30 -0400

    update

--- test/qttest.jl
+++ test/qttest.jl
@@ -10,16 +10,17 @@ defineMacro("__linux")
 addHeaderDir(qtincdir, kind = C_System)
 addHeaderDir(QtWidgets, kind = C_System)
 
-dlopen(joinpath(qtlibdir,"libQt5Core.so"))
-dlopen(joinpath(qtlibdir,"libQt5Gui.so"))
+dlopen(joinpath(qtlibdir,"libQt5Core.so"), RTLD_GLOBAL)
+dlopen(joinpath(qtlibdir,"libQt5Gui.so"), RTLD_GLOBAL)
+dlopen(joinpath(qtlibdir,"libQt5Widgets.so"), RTLD_GLOBAL)
 
 cxxinclude("QApplication", isAngled=true)
-#cxxinclude("QtGui/qmessagebox.h")
+cxxinclude("QMessageBox")
 
 
 x = Ptr{Uint8}[pointer("julia"),C_NULL]
 app = @cxx QApplication(int32(0),pointer(x))
 
-#@cxx QMessageBox::about(cast(C_NULL,pcpp"QWidget"), pointer("Hello World"), pointer("This is a QMessageBox!"))
+@cxx QMessageBox::about(cast(C_NULL,pcpp"QWidget"), pointer("Hello World"), pointer("This is a QMessageBox!"))
 
 @cxx app->exec()
